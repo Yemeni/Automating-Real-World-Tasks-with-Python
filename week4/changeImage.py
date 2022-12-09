419c530f75943af029093fca7daa0efbac750e4a
Yousef <yhadhr@gmail.com>
2022-12-09 19:33:20 +0400

    preparing class to write first challenge of week 4

---
+++ week4/changeImage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from PIL import Image
+
+
+
+
+# TODO: You will be using the PIL library to update all images within ~/supplier-data/images directory to the following specifications:
+"""
+Size: Change image resolution from 3000x2000 to 600x400 pixel
+Format: Change image format from .TIFF to .JPEG
+"""
+
+
+# TODO: After processing the images, save them in the same path ~/supplier-data/images, with a JPEG extension.
