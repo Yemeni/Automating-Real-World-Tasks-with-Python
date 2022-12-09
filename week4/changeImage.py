55cb9c2248a1b348687d29f5c94aac2c9ac0e99d
Yousef <yhadhr@gmail.com>
2022-12-09 19:43:41 +0400

    some test objects

--- week4/changeImage.py
+++ week4/changeImage.py
@@ -4,7 +4,26 @@ import os
 import sys
 from PIL import Image
 
+# listing all the images variables
+# imagesDir = '~/supplier-data/images' # linux
+imagesDir = 'supplier-data/images' # windows
+imagePath = os.path.expanduser(imagesDir)
+images = os.listdir(imagePath)
+sourceFormat = "tiff"
+distFormat = "jpeg"
 
+
+for fp in images:
+    # Split the extension from the path and normalise it to lowercase.
+    ext = os.path.splitext(fp)[-1].lower()
+
+    # Now we can simply use == to check for equality, no need for wildcards.
+    if ext == ".tiff":
+        print (fp, "is an tiff that we will change!")
+    elif ext == ".jpeg":
+        print (fp, "is a jpeg that we already changed!")
+    else:
+        print (fp, "is an unknown file format.")
 
 
 # TODO: You will be using the PIL library to update all images within ~/supplier-data/images directory to the following specifications:
